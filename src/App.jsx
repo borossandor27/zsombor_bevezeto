c1cd1140c8f8e8b8f2e7a6c856ece60833159794
borossandor27 <boros.sanyi@gmail.com>
2024-07-18 23:04:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <header className="App-header">
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+//-- https://www.youtube.com/watch?v=Xwubbu2D7J0&t=476s
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
